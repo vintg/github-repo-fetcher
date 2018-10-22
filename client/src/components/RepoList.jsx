5a38739cb0b3439f33a3c5b074b6d73bea8564e0
Vincent L <liberty.sky87@gmail.com>
2018-10-21 19:51:39 -0700

    save

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -7,7 +7,22 @@ const RepoList = (props) => (
     There are {props.repos.length} repos.
   </div>
   <div className="repo-list">
+   <table>
+      <thead>
+        <tr>
+          <th>Owner</th>
+          <th>Repository</th>
+          <th>Description</th>
+          <th>Created</th>
+          <th>Last Updated</th>
+          <th>Size</th>
+          <th>Forks</th>
+          <th>Issues</th>
+          <th>Followers</th>
+        </tr>
+      </thead>
     props.repos.map(repo => <RepoListEntry key= {repo.id} repo={repo}/>
+   </table>
   </div>
 )
 
